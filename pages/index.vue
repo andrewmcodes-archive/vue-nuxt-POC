26ed2a2557866a666c354d1eda0197cb8554dd01
andrewmcodes <andrew.mason@n2pub.com>
2018-09-28 21:28:47 -0400

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,21 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        onshore-studios
-      </h1>
-      <h2 class="subtitle">
-        Onshore Studios Production Website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <contact />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Contact from '~/components/Contact.vue'
 export default {
   components: {
-    Logo
+    Contact
   }
 }
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
 .title {
