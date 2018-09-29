26be025b6b48f5f34cd3914b6180a4f9f53f7a08
andrewmcodes <andrew.mason@n2pub.com>
2018-09-29 04:11:48 -0400

    wiiiip

--- components/Contact.vue
+++ components/Contact.vue
@@ -73,7 +73,7 @@ export default {
         .join('&')
     },
     handleSubmit() {
-      fetch('/', {
+      fetch('', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: this.encode({ 'form-name': 'contact', ...this.form })
