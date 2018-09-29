b06ca830e07c766673d8de7e29d1a543906bbc60
andrewmcodes <andrew.mason@n2pub.com>
2018-09-28 21:42:39 -0400

    fix failing build

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: ['~/assets/css/tailwind.css', '~/css/main.css'],
+  css: ['~/assets/css/tailwind.css', '~/assets/css/main.css'],
 
   /*
    ** Plugins to load before mounting the App
